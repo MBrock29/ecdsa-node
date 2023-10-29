f56703d75e47821ec102db9f1d0c11f1d3802e41
mbrock29 <mbrock19@gmail.com>
2023-10-29 13:41:34 -0500

    Public key

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -4,3 +4,8 @@ const { toHex } = require("ethereum-cryptography/utils");
 const privateKey = secp.secp256k1.utils.randomPrivateKey();
 
 console.log("priv key", toHex(privateKey));
+
+const publicKey = secp.secp256k1.getPublicKey(privateKey);
+const abbrKey = publicKey.slice(-20);
+
+console.log("public key", toHex(abbrKey));

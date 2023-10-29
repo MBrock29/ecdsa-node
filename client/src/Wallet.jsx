4ffc515ae4d44a5b7de073549210f05f65ad19ea
mbrock29 <mbrock19@gmail.com>
2023-10-29 13:28:45 -0500

    FE changes

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,4 +1,5 @@
 import server from "./server";
+import { useState } from "react";
 
 function Wallet({ address, setAddress, balance, setBalance }) {
   async function onChange(evt) {
@@ -13,17 +14,27 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       setBalance(0);
     }
   }
-
+  const [accountLoggedIn, setAccountLoggedIn] = useState("");
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input
+          placeholder="Type an address, for example: 0x1"
+          value={address}
+          onChange={onChange}
+        ></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
+      <div>
+        <button onClick={() => setAccountLoggedIn("0x1")}>0x1</button>
+        <button onClick={() => setAccountLoggedIn("0x2")}>0x2</button>
+        <button onClick={() => setAccountLoggedIn("0x3")}>0x3</button>
+      </div>
+      <div>Logged in as: {accountLoggedIn}</div>
     </div>
   );
 }
